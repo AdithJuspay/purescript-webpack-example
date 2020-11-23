06f8c75d355917f7e04e18dfdf2ac94c057c0c9c
Igor Nilovich Ramazanov <igor.ramazanov@protonmail.com>
2020-11-23 23:36:06 +0300

    Tooling and general improvements 1. Favicons 2. Print purs-loader debug logs 3. PureScript and JavaScript source maps (not ideal) 4. Serve the bundle by webpack-dev-server without storing on disk 5. Setup PureScript fatal warnings 6. PureScript formatter - can be used to "format file on save" in any editor or run as a separate process listening for file changes 7. Format checking - can be used in a git pre-commit hook and/or in the Continuous Integration 8. Automatic fixing of warning

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
-
 const isWebpackDevServer = process.argv.some(a => path.basename(a) === 'webpack-dev-server');
-
 const isWatch = process.argv.some(a => a === '--watch');
-module.exports = {
-    
+
+module.exports = {    
     entry : "./action.js",
     output: {
-      path: __dirname,
-      pathinfo: true,
       filename: 'bundle.js'
     },
-    mode :  "development",
-    target: 'web',
+    bail: true, // fail-fast
+    devtool: "source-map", // JS source-maps
+    mode : "development", // don't minify the output bundle
     module: {
         rules: [
           {
@@ -24,20 +21,32 @@ module.exports = {
                 options: {
                   src: [
                     'bower_components/purescript-*/src/**/*.purs',
-                    'src/**/*.purs'
+                    'src/**/*.purs',
+                    'test/**/*.purs'
                   ],
-                  bundle: false,
-                  psc: 'psa',
+                  bundle: false, // don't insert automatic invocation of the 'main' function, build PureScript as a module for 'require'/'import'
+                  psc: 'psa', // 'psa' (alias for 'purescript-psa') is a wrapper around psc (alias for 'purs') for better support of warnings
                   watch: isWebpackDevServer || isWatch,
-                  pscIde: false
+                  pscIde: false,
+                  pscArgs: {
+                  	codegen: 'js,sourcemaps', // generate both the JavaScript code + Source maps
+                  	isLib: 'bower_components', // for 'censorLib'
+                  	stash: true, // keep warnings between incremental compilation into .psa-stash (must be gitignored)
+                  	censorLib: true, // ignore warnings in libraries, use 'isLib' to determine if the code is library or not
+                  	censorSrc: false, // don't ignore user code warnings (everything what is not in the 'isLib' directory, normally 'src' and 'test' directories)
+                  	strict: true, // warnings cause compilation error
+                  }
                 }
               }
             ]
           }
         ]
     },
-    resolve: {
-        modules: [ 'node_modules', 'bower_components' ],
-        extensions: [ '.purs', '.js']
-      }
+    devServer: {
+       port: 8080,
+       host: 'localhost',
+       inline: false,
+       publicPath: '/',
+       writeToDisk: false
+    }
 }
